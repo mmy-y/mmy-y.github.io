f1def4974c6155664eeeed97c94533845d35f4fe
mmy-y <2709106630@qq.com>
2023-06-14 02:02:40 +0800

    Site updated: 2023-06-14 02:02:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["archives/c74c8b34.html","archives/54c35e39.html","archives/50a9f1de.html","archives/428e76ab.html","archives/50a7fc42.html","archives/273541a6.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["archives/c74c8b34.html","archives/5a9296a.html","archives/54c35e39.html","archives/50a9f1de.html","archives/428e76ab.html","archives/50a7fc42.html","archives/273541a6.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
